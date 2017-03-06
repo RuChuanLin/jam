584ae861502d1ab8aa24997133d5c64014ec2762
River <ac97631@gmail.com>
2017-03-06 21:41:05 +0800

    3/6 2

---
+++ src/main/java/_01_member/controller/LoginServlet.java
@@ -0,0 +1,71 @@
+package _01_member.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import _01_member.model.Member;
+import _01_member.model.MemberDAO;
+import _01_member.model.MemberHBN;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		String account = request.getParameter("account");
+		String password = request.getParameter("password");
+		MemberDAO dao = new MemberHBN();
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		Map<String, Object> map = new HashMap<>();
+		Gson gson = new Gson();
+		
+		// 檢查帳號是否輸入及是否存在
+		if (!dao.idExists(account)) {
+			map.put("loginSuccess", false);
+			System.out.println("帳號不存在");
+		}
+		
+		// 檢查密碼是否輸入及是否正確
+		if (!dao.checkPassword(account, password)) {
+			map.put("loginSuccess", false);
+			System.out.println("密碼錯誤");
+		}
+
+		//有問題
+		if (!map.isEmpty()) {
+			pw.write(new Gson().toJson(map));
+			pw.flush();
+			System.out.println("登入失敗");
+			return;
+		}
+		
+		//沒問題
+		Member mb = dao.getMemberByAccount(account);
+		int id = mb.getUserId();
+		
+		pw.write(new Gson().toJson(mb));
+		System.out.println(mb);
+		pw.flush();
+		System.out.println("成功登入");
+		session.setAttribute("LoginId", id);
+		session.setAttribute("LoginAcc", account);
+		return;
+	}
+
+}
---
+++ src/main/java/_01_member/controller/UpdatePerson.java
@@ -0,0 +1,68 @@
+package _01_member.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Clob;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialClob;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import _01_member.model.Member;
+import _01_member.model.MemberDAO;
+import _01_member.model.MemberHBN;
+
+/**
+ * Servlet implementation class UpdatePerson
+ */
+@WebServlet("/updatePerson")
+public class UpdatePerson extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+		
+		String account = request.getParameter("account");
+		String email = request.getParameter("email");
+		String alias = request.getParameter("alias");
+		String pic = request.getParameter("pic");
+		String intro = request.getParameter("intro");
+		String instruments[] = request.getParameterValues("instruments[]");
+		String instru = "";
+		System.out.println(instruments);
+		if (instruments != null) {
+			for (int i = 0; i < instruments.length; i++) {
+				instru += instruments[i] + " \\ ";
+			}
+			instru = instru.substring(0, instru.length() - 3);
+		}
+		System.out.println(instru);
+		MemberDAO memberDao = new MemberHBN();
+		Member mem = new Member(account, "", instru, true, email, true, alias, pic, intro);
+		memberDao.updateMember(mem);
+		Member memReturn = new Member(instru, true, email, true, alias, pic, intro);
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+		String json = gson.toJson(memReturn);
+
+		System.out.println(json);
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
+
+	}
+
+}
