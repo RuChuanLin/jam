93ad33ee8375b462eaca485bebf959ab6ec607c0
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-03-18 20:44:41 +0800

    除錯

--- WebContent/_997_framework/js/main_altering.js
+++ WebContent/_997_framework/js/main_altering.js
@@ -64,7 +64,6 @@ jQuery(document).ready(function($) {
     regSubmit.on("click", onSignupClick);
     loginSubmit.on("click",onLoginClick);
     logoutButton.on("click", onLogoutClick);
-	
 
     signupEmail.on('keyup', function() {
         console.log(account);
@@ -126,26 +125,16 @@ jQuery(document).ready(function($) {
 				}				
 			})
 
-        
-
     }
 
-	
     function onLogoutClick() {
-        //登出畫面，以下寫程式碼
 		mem.logout();
 		//reload的時候就會觸發 $(document).ready裡的方法重新判斷登入狀態。
         window.location.reload(true);
         return;
     }
 
-
-
-    //-------------------------------------
-
-    //open sign-up form
     navRight.on('click', '.signup', signup_selected);
-    //open login-form form
     navRight.on('click', '.login', login_selected);
 
 
@@ -154,7 +143,7 @@ jQuery(document).ready(function($) {
             close_modal();
         }
     });
-    //close modal when clicking the esc keyboard button
+    //close modal when pressing the keyboard esc button
     $(document).keyup(function(event) {
         if (event.which == '27') {
             formModal.removeClass('is-visible');
@@ -179,30 +168,66 @@ jQuery(document).ready(function($) {
         event.preventDefault();
         login_selected();
     });
-    //註冊密碼不符合規則
-
-
-    signupPassword.on('keyup', this, submit_able);
+    //確認註冊密碼是否符合規則
+    signupPassword.on('keyup',function(){
+		password_rule(this.value);
+	});
 
     //註冊確認密碼不一致
-    passwordConfirm.on('keyup', this, password_different);
-    passwordConfirm.on('keyup', this, submit_able);
-
-    //勾選同意條款
-    termsAccept.on('click', this, submit_able);
-
-	index_methods.chkRegPw=function(){
+    passwordConfirm.on('keyup',function(){
+		password_different([this.value,this.value]);
 		
-	}
+	});
+	
+    termsAccept.on('click', function(){	
+        if ($(this).checked && password_different([signupPassword.val(),passwordConfirm.val()])) {
+            regSubmit.removeAttr('disabled');
+        } else {
+            regSubmit.attr('disabled', 'disabled');
+			}	
+	});
 
+	
+	function password_rule() {
+        var rule = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]).{7,15}$/;
+        if (rule.test(this.value)) {
+            $(this).next('span').removeClass('is-visible');
+            return true;
+        } else {
+            $(this).next('span').addClass('is-visible');
+            return false;
+        }
+
+    }
+
+    function password_different(val) {
+		var pwsu=signupPassword.val();
+		var pwcfn=confirmPassword.val();
+		
+		if(pwsu.length==pwcfn.length){
+			if(!password_rule(pwsu) || !password_rule(pwcfn)){return;}
+			if (val[0]. === val[1]) {
+			passwordConfirm.next('span').removeClass('is-visible');
+            return false;
+			} else {
+				passwordConfirm.next('span').addClass('is-visible');
+				return true;}
+		}else{
+			return false;	}
+    }
+	
+
+    }
+    //登入帳密錯誤
+    function error_idps() {
+        $('#remember-me').append('<div class="error-IdPs"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
+    }
 
 
     //驗證畫面按確認後回到主畫面
     $('.confirm-button').on('click', function() {
         $('.confirm-success').toggleClass('is-visible');
     });
-
-
 
     function login_selected() {
         navRight.removeClass('is-visible');
@@ -232,65 +257,6 @@ jQuery(document).ready(function($) {
         formSignup.removeClass('is-selected');
         formForgotPassword.addClass('is-selected');
     }
-
-    function terms_Ischecked() {
-        if (termsAccept.prop("checked")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function submit_able() {
-        if (terms_Ischecked() && password_rule() && password_different()) {
-            regSubmit.removeAttr('disabled');
-        } else {
-            regSubmit.attr('disabled', 'disabled');
-        }
-    }
-
-
-    function password_rule() {
-        var rule = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]).{7,15}$/;
-        if (rule.test(signupPassword.val())) {
-            signupPassword.next('span').removeClass('is-visible');
-            return true;
-        } else {
-            signupPassword.next('span').addClass('is-visible');
-            return false;
-        }
-
-    }
-
-    function password_different() {
-        if (signupPassword.val() != passwordConfirm.val()) {
-
-            passwordConfirm.next('span').addClass('is-visible');
-            return false;
-        } else {
-            passwordConfirm.next('span').removeClass('is-visible');
-            return true;
-        }
-    }
-
-    //登入帳密錯誤
-    function error_idps() {
-        $('#remember-me').append('<div class="error-IdPs"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
-    }
-    // ------------------ajax+servlet整合的畫面function----------------------
-    //登入後nav-bar右上角的顯示
-
-    var test1 = $('.test-btn1');
-    var test2 = $('.test-btn2');
-    var navbarRight = $('.navbar-right');
-
-    test1.click(index_methods.login_Nav);
-    test2.click(index_methods.logout_Nav);
-
-
-
-    //登入帳密錯誤動畫
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
 
 });
