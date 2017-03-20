42db6964e96af82ad93d898449464c8b7f6c35a3
River <ac97631@gmail.com>
2017-03-20 18:25:44 +0800

    3/20 格非格合版

---
+++ WebContent/_997_framework/js/croppie_inst.js
@@ -0,0 +1,81 @@
+//這個腳本是專為member.html撰寫，沒有考慮移植性。
+
+
+var cropper = undefined;
+
+$(document).ready(setup);
+
+function setup() {
+    $("#fake-update-input").on("change", startCroppie);
+    $("#crop").on("click", saveCroppie);
+    $("#crop-cancel").on("click", closeCroppie);
+
+    function setupCroppie() {
+        var opts = {
+            viewport: { width: 140, height: 140, type: "circle" },
+            showZommer: true
+        }
+        cropper = new Croppie($("#cropArea")[0], opts);
+
+
+
+    }
+
+    function closeCroppie(){
+        $("#cropArea").css("display", "none");
+    	  cropper.destroy();
+    }
+
+
+
+    function startCroppie(event) {
+        setupCroppie();
+        console.log("on");
+        $("#cropArea").css("display", "block");
+        fr = new FileReader();
+        fr.onload = function(e) {
+            var pic = {
+                url: e.target.result,
+                points: [200, 200, 400, 400],
+                orientation: 1
+            };
+            cropper.bind(pic);
+        };
+        fr.readAsDataURL(event.target.files[0]);
+        $(this).val("");
+    }
+
+
+    function saveCroppie() {
+        console.log("no");
+        var opts = {
+            type: "base64",
+            format: "jpeg",
+            size: "viewport",
+            quality: 1,
+            circle: true
+        }
+        cropper.result(opts).then(function(e) {
+            var img = new Image();
+            img.src = e;
+            img.onload = function() {
+                var cns = $("#cropboard")[0];
+                var ctx = cns.getContext("2d");
+                ctx.clearRect(0, 0, cns.width, cns.height);
+                ctx.beginPath();
+                ctx.arc(70, 70, cns.width / 2.25, Math.PI * 2, 0, false);
+                ctx.clip("evenodd");
+                ctx.drawImage(this, 0, 0, 140, 140);
+                var rst = cns.toDataURL();
+                // $("#member-pic").attr("src", rst);
+                $("#preview-pic").attr("src", rst);
+                cropper.destroy();
+            }
+        });
+        $("#cropArea").css("display", "none");
+    }
+
+
+
+
+}
